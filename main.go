aa8b490c53e51c60df2cd1d615c50705d47b505a
take <chantake33@gmail.com>
2019-09-09 13:38:33 +0900

    update

--- main.go
+++ main.go
@@ -18,12 +18,17 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", index)
+	mux.HandleFunc("/dump", dumprequest)
+	mux.HandleFunc("/slow", slowrequest)
 	log.Fatal(http.ListenAndServe(":8081", mux))
 
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	// hello
+	io.WriteString(w, "This is echoecho service\n")
+}
+
+func dumprequest(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "This is echoecho service\n")
 
 	dump, _ := httputil.DumpRequest(r, true)
@@ -32,6 +37,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "\n")
 
 	// Request
+	url = url + "/dump"
 	req, _ := http.NewRequest("GET", url, nil)
 
 	dumpReq, _ := httputil.DumpRequestOut(req, true)
@@ -48,4 +54,20 @@ func index(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "===DumpResponse===\n")
 	io.WriteString(w, string(dumpResp))
 
+}
+
+func slowrequest(w http.ResponseWriter, r *http.Request) {
+
+	io.WriteString(w, "This is echoecho service\n")
+	// Request
+	url = url + "/wait"
+	req, _ := http.NewRequest("GET", url, nil)
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dumpResp, _ := httputil.DumpResponse(resp, true)
+	io.WriteString(w, "===DumpResponse===\n")
+	io.WriteString(w, string(dumpResp))
 }
